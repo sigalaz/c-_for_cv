e92df01941f78e950500d5fe7b037f3829a34bef
sigalaz <sigalaz@miuandes.cl>
2022-07-22 10:56:31 -0400

    added some simple (unfinished) example to understand tuple and array types

---
+++ homeworks/hw3/help/tuple_example.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <tuple>
+#include <array>
+
+int main() {
+	std::tuple<int, char> foo(10, 'x');
+	std::tuple<int, char> foo(20, 'y');
+	std::tuple<int, char> foo(30, 'z');
+
+
+	std::array
+
+	//std::cout << std::get<0>(foo) << std::endl;
+
+
+}
