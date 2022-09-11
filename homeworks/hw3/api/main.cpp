528f8f2f296feb5caba8af9c01259f73e9be0036
sigalaz <sigalaz@miuandes.cl>
2022-09-10 21:17:44 -0400

    added CreateImageBrowser functionality

--- homeworks/hw3/api/main.cpp
+++ homeworks/hw3/api/main.cpp
@@ -1,22 +1,31 @@
 #include "../api/html_writer.hpp"
+#include "../api/image_browser.hpp"
+
 #include <iostream>
 
-#include <array>
-#include <tuple>
-#include <vector>
-
-#include "fmt/args.h"
-
 int main() {
+
+  image_browser::ScoredImage img1("data/000000.png", 0.05);
+  image_browser::ScoredImage img2("data/000100.png", 0.08);
+  image_browser::ScoredImage img3("data/000000.png", 0.05);
+
+  image_browser::ImageRow a = {img1, img1, img1};
+  image_browser::ImageRow b = {img2, img2, img2};
+
+  std::vector<image_browser::ImageRow> img_set = {a, b};
+
+  // image_browser::CreateImageBrowser("Hellos", "style.css", img_set);
 
   html_writer::OpenDocument();
   html_writer::AddTitle("Hello, this html file was generated from C++");
   html_writer::AddCSSStyle("style.css");
   html_writer::OpenBody();
-  html_writer::OpenRow();
-  html_writer::AddImage("data/000000.png", 0.05, false);
-  html_writer::CloseRow();
-
+  image_browser::AddFullRow(a, false);
+  /*
+      html_writer::OpenRow();
+      html_writer::AddImage("data/000000.png", 0.05, false);
+      html_writer::CloseRow();
+  */
   html_writer::CloseBody();
   html_writer::CloseDocument();
 
