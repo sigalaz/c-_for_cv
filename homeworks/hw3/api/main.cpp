92d75afb6a72cd199b444e327713d42fa397a6f4
sigalaz <sigalaz@miuandes.cl>
2023-02-02 10:32:26 -0300

    added all 9 images for testing the same quantity as the example given

--- homeworks/hw3/api/main.cpp
+++ homeworks/hw3/api/main.cpp
@@ -7,12 +7,21 @@ int main() {
 
   image_browser::ScoredImage img1("data/000000.png", 0.05);
   image_browser::ScoredImage img2("data/000100.png", 0.08);
-  image_browser::ScoredImage img3("data/000000.png", 0.05);
+  image_browser::ScoredImage img3("data/000200.png", 0.05);
 
-  image_browser::ImageRow a = {img1, img2, img3};
-  image_browser::ImageRow b = {img2, img2, img2};
+  image_browser::ScoredImage img4("data/000300.png", 0.05);
+  image_browser::ScoredImage img5("data/000400.png", 0.08);
+  image_browser::ScoredImage img6("data/000500.png", 0.05);
 
-  std::vector<image_browser::ImageRow> img_set = {a, b};
+  image_browser::ScoredImage img7("data/000600.png", 0.05);
+  image_browser::ScoredImage img8("data/000700.png", 0.08);
+  image_browser::ScoredImage img9("data/000800.png", 0.55);
+
+  image_browser::ImageRow a = {img9, img2, img3};
+  image_browser::ImageRow b = {img4, img5, img6};
+  image_browser::ImageRow c = {img7, img8, img9};
+
+  std::vector<image_browser::ImageRow> img_set = {a, b, c};
 
   image_browser::CreateImageBrowser("Hellos", "style.css", img_set);
 
