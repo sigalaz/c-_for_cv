6b17f71b1fc0393b4d4cc50de4cf63135c5dae1f
sigalaz <sigalaz@miuandes.cl>
2023-02-17 14:33:10 -0300

    deleted struct type

--- homeworks/hw4/src/ipb_vector.cpp
+++ homeworks/hw4/src/ipb_vector.cpp
@@ -1,12 +1,5 @@
-#include <array>
 #include <iostream>
+#include <string>
+#include <vector>
 
-template <typename T>
-
-struct vector {
-  T first_vector_type;
-
-  int vector_type;
-};
-
-vector<int> a;
+int main() { std::vector<int> d; }
